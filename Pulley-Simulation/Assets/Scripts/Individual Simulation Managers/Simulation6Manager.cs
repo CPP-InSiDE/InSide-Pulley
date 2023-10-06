2cedddc25d9d21d31d954227ea96def20f68653d
Ratske99 <sethlaske9999@gmail.com>
2023-10-06 13:33:50 -0700

    Adding simulation 6

---
+++ Pulley-Simulation/Assets/Scripts/Individual Simulation Managers/Simulation6Manager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Simulation6Manager : SimulationManager
+{
+    [SerializeField] private PhysicsObject inputBlockA;
+    [SerializeField] private PhysicsObject inputBlockB;
+    [SerializeField] private PhysicsObject inputBlockC;
+    [SerializeField] private PhysicsObject pulleyD;
+    [SerializeField] private PhysicsObject pulleyC;
+
+    private void Start()
+    {
+        defaultPredictionColor = userPredictionBlockSpriteRenderer.color;
+    }
+
+
+    protected override void CalculateOutputBlocks()
+    {
+        //SetBaseBlock();
+        float velocityMagnitude = (   (-.5f * inputBlockA.velocity.magnitude * inputBlockA.CheckOriginalVelocityDirection())
+                                    + (-.5f * inputBlockB.velocity.magnitude * inputBlockB.CheckOriginalVelocityDirection())
+                                    + (-1.5f * inputBlockC.velocity.magnitude * inputBlockC.CheckOriginalVelocityDirection()) );
+        solutionBlock.SetVelocityMagnitude(velocityMagnitude);
+
+
+        float accelerationMagnitude = (    (-.5f * inputBlockA.acceleration.magnitude * inputBlockA.CheckOriginalAccelerationDirection())
+                                         + (-.5f * inputBlockB.acceleration.magnitude * inputBlockB.CheckOriginalAccelerationDirection())
+                                         + (-1.5f * inputBlockC.acceleration.magnitude * inputBlockC.CheckOriginalAccelerationDirection()));
+        solutionBlock.SetAccelerationMagnitude(accelerationMagnitude);
+
+
+        pulleyD.SetVelocityMagnitude(-.5f * velocityMagnitude);
+        pulleyD.SetAccelerationMagnitude(-.5f * accelerationMagnitude);
+
+        pulleyC.SetVelocityMagnitude(2f * inputBlockB.velocity.magnitude * inputBlockB.CheckOriginalVelocityDirection());
+        pulleyC.SetAccelerationMagnitude(2f * inputBlockB.acceleration.magnitude * inputBlockB.CheckOriginalAccelerationDirection());
+    }
+
+   
+}
